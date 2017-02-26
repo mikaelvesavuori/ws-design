47259d57deffe301d28764234e6cf85c85320201
Mikael Vesavuori <mikael.vesavuori@sogeti.com>
2017-02-26 22:47:42 +0100

    Updating

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ws-design
+# Workshop: Komponentbaserad design – BEM, Sass och designsystem
+Vi går mot en tid av komponenttänkande snarare än sidtänkande. Denna förändring ställer väldigt många tidigare uppfattningar och verktyg på ända, både i det grafiska arbetet såväl som inom implementationen av detta i kod. Under denna halvdag får ni möjlighet att bekanta er med en process för komponentbaserad design (från styleguide till handoff), arbeta med Sass/BEM/PostCSS/Stylelint för att skriva kod för en komponent, samt regressionstesta och dokumentera er CSS.
+
+## Mål
+Kunna skapa visuella komponenter, utföra dessa i CSS enligt en team-standard samt förstå prestandan bakom
--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,58 @@
-const gulp = require("gulp");
-const browsersync = require("browser-sync");
-const sass = require("gulp-sass");
-
-gulp.task("sass", function() {
-	return gulp.src("main.scss")
-		.pipe(sass())
-		.pipe(gulp.dest(""));
-});
-
-
-/*
-const config = require("../config.js");
+const config = require("./config.js");
 
 const gulp = require("gulp");
-const browsersync = require("browser-sync");
 const plumber = require("gulp-plumber");
 const sourcemaps = require("gulp-sourcemaps");
+const browsersync = require("browser-sync");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const scss = require("postcss-scss");
+const reporter = require("postcss-reporter");
 const precss = require("precss");
 const autoprefixer = require("autoprefixer");
-const flexibility = require("postcss-flexibility");
-const ofi = require("postcss-object-fit-images");
-//const doiuse = require("doiuse");
-//const perfectionist = require("perfectionist");
-const gulpFilter = require("gulp-filter");
-const rename = require("gulp-rename");
+const stylelint = require("stylelint");
+const stylefmt = require("gulp-stylefmt");
 
-gulp.task("sass", function () {
+gulp.task("default", ["watch"], function() { });
+
+gulp.task("watch", ["browsersync"], function() { });
+
+gulp.task("browsersync", function() {
+	browsersync(config.browsersync);
+
+	gulp.watch("*.{sass,scss}", ["sass"]);
+	gulp.watch("*.{css,html}", browsersync.reload);
+});
+
+
+gulp.task("sass", function() {
 	const processors = [
 		precss(config.precss.options),
-		autoprefixer(config.autoprefixer.options),
-		flexibility(),
-		ofi()
-		//doiuse(config.doiuse.options),
-		//perfectionist(config.perfectionist.options)
+		autoprefixer(config.autoprefixer.options)
 	];
 
-	const filter = gulpFilter(["*.css", "!*.map"], { restore: true });
-	browsersync.notify("Compiling Sass");
-
-	return gulp.src(config.sass.src)
-		.pipe(plumber())
+	return gulp.src("main.scss")
 		.pipe(sourcemaps.init())
-		.pipe(sass.sync().on("error", sass.logError))//, browsersync.notify("Error in Sass")))
+		.pipe(sass.sync().on("error", sass.logError))
 		.pipe(postcss(processors, {syntax: scss}))
-		//.pipe(filter)
-		.pipe(sourcemaps.write("maps")) //, { includeContent: false, sourceRoot: "Static/sass/"}))
-		//.pipe(filter.restore)
-		//.pipe(rename({dirname: ""}))
+		.pipe(sourcemaps.write("maps"))
 		.pipe(gulp.dest(config.sass.dest))
 		.pipe(browsersync.stream());
 });
-*/
+
+gulp.task("stylelint", function() {
+	const processors = [
+		stylelint(config.stylelint),
+		reporter(config.reporter)
+	];
+
+	return gulp.src(config.stylelint.src)
+		.pipe(plumber())
+		.pipe(postcss(processors, {syntax: scss}))
+});
+
+gulp.task("stylefmt", function () {
+	return gulp.src(config.stylefmt.src)
+		.pipe(stylefmt(config.stylelint))
+		.pipe(gulp.dest(config.stylefmt.dest));
+});
